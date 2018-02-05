1fd6e18bfa896e2a24bccb013af121adc3d76c21
Shashi Prasad <shashi.prsd@yahoo.com>
2018-02-05 16:58:07 +0530

    Commit

--- udemy/src/main/java/basics/BrowserInvocation.java
+++ udemy/src/main/java/basics/BrowserInvocation.java
@@ -1,6 +1,10 @@
 package basics;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -14,7 +18,7 @@ public class BrowserInvocation {
 		
 		driver= new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.close();
+		//driver.close();
 		
 	}
 	public void launchChrome(){
@@ -40,9 +44,27 @@ public class BrowserInvocation {
 		// TODO Auto-generated method stub
 		BrowserInvocation BI= new BrowserInvocation();
 		//BI.launchChrome();
-		//BI.launchFirfox();
-		BI.launchIE();
-
+		BI.launchFirfox();
+		//BI.launchIE();
+		driver.get("http://www.airindia.in/");
+		System.out.println(driver.getTitle());
+		
+		
+		WebElement datePicker = driver.findElement(By.id("_depdateeu1"));
+		datePicker.click();
+		
+		List<WebElement> date = driver.findElements(By.xpath(".//a[@class='ui-state-default']"));
+		int size = driver.findElements(By.className("ui-state-default")).size();
+		System.out.println(size);
+		for (int i = 0; i < size; i++) {
+			String dateText = driver.findElements(By.xpath(".//a[@class='ui-state-default']")).get(i).getText();
+			System.out.println(dateText);
+			if (dateText.equalsIgnoreCase("23")) {
+				driver.findElements(By.xpath(".//a[@class='ui-state-default']")).get(i).click();
+				break;
+			}
+		}
+		
 	}
 
 }
